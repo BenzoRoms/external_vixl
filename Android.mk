8aa13997f7edad57d4f6903e07d4d57127e4828a
armvixl <vixl@arm.com>
2016-05-10 13:57:58 +0100

    VIXL Release 1.13
    Refer to the README.md and LICENCE files for details.

--- Android.mk
+++ Android.mk
@@ -94,6 +94,7 @@ vixl_test_files := \
   test/test-invalset.cc \
   test/test-runner.cc \
   test/test-simulator-a64.cc \
+  test/test-trace-a64.cc \
   test/test-utils-a64.cc
 
 vixl_cpp_flags := \
